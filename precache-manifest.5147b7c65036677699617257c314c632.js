af886f5abba33d85ee04f0514cf3cd284ee6c61a
TiepPt <tieppt07@gmail.com>
2020-09-29 15:29:17 +0700

    Updates

---
+++ precache-manifest.5147b7c65036677699617257c314c632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce3e242d314a8c69d61faccd22b0ab2",
+    "url": "/reactjs-hw/index.html"
+  },
+  {
+    "revision": "8bbe5d42a3854521c586",
+    "url": "/reactjs-hw/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0fb3302416749050056b",
+    "url": "/reactjs-hw/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8bbe5d42a3854521c586",
+    "url": "/reactjs-hw/static/js/2.4cc5b1d9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactjs-hw/static/js/2.4cc5b1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb3302416749050056b",
+    "url": "/reactjs-hw/static/js/main.2f6330f8.chunk.js"
+  },
+  {
+    "revision": "4e4aaeb05e318c46a142",
+    "url": "/reactjs-hw/static/js/runtime-main.f21d3c26.js"
+  }
+]);
